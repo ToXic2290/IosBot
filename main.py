8d2b0af4305e14015da505e013aabcdc149229ba
ToXic2290 <87765394+ToXic2290@users.noreply.github.com>
2022-08-27 00:56:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def start(message):
     btn25 = types.KeyboardButton("❤️‍🔥 WhatsApp ❤️‍🔥")
     markup.add(btn25, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn14, btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23)
     markup.add(btn13)
-    bot.send_message(message.chat.id, text='''Выбери любое приложение из списка ниже'''.format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text='''🙃 Приветствую'''message.chat.username'''!'''.format(message.from_user), reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def func(message):
