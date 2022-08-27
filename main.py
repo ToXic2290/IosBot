679f401461490dac392f97f826e0e4c573e7e81d
ToXic2290 <derzkiy8233@gmail.com>
2022-08-27 16:53:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,7 @@ def start(message):
     btn21 = types.KeyboardButton("⌨️ Keyboard ⌨️")
     btn22 = types.KeyboardButton("📒 Notes 📒")
     btn23 = types.KeyboardButton("📩 Notifications  📩")
-    btn25 = types.KeyboardButton("❤️‍🔥 WhatsApp ❤️‍🔥")
-    btn25 = types.KeyboardButton(buttons.whatsapp)
+    btn25 = types.KeyboardButton(buttons.wwhatsapp)
     markup.add(btn25, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn14, btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23)
     markup.add(btn13)
     bot.send_message(message.chat.id, text='''🙃 Приветствую дорогой пользователь.
