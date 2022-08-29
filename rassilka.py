41efffae0cfc9ec235c7ee84e04e9df6b685a06b
ToXic2290 <derzkiy8233@gmail.com>
2022-08-29 23:17:05 +0600

    Update rassilka.py

--- rassilka.py
+++ rassilka.py
@@ -1,8 +1,7 @@
-joinedFile = open("IosBot/users.db", 'r')
+joinedFile = open("C:/IosBot/users.db", 'r')
 joinedUsers = set ()
 
 @bot.message_handler(commands=["special"])
 def mess(message):
 	for user in joinedUsers:
 		bot.send_message(user, message.text[message.text.find(' '):])
-		
