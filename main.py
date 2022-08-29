27b02caf9c8bbcd8a44d1c312943f1d25c09e2a9
ToXic2290 <derzkiy8233@gmail.com>
2022-08-29 23:32:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from telebot import types # для указание типов
 import caption
 import files
 import sqlite3
+import configure
 
 # Авторизация в бота
-bot = telebot.TeleBot("5400897291:AAGCWphbUiKx7r1ntjHQNfL75WaWCRk6cvA")
-
+client = telebot.TeleBot(configure.config['token'])
 
 
 # Команда старт и запись айди в базу данных
