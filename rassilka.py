c51423b8d76502502398a768e54dea778e08d6b6
ToXic2290 <derzkiy8233@gmail.com>
2022-08-29 23:21:33 +0600

    Delete rassilka.py

--- rassilka.py
+++
@@ -1,7 +0,0 @@
-joinedFile = open("C:/IosBot/users.db", 'r')
-joinedUsers = set ()
-
-@bot.message_handler(commands=["special"])
-def mess(message):
-	for user in joinedUsers:
-		bot.send_message(user, message.text[message.text.find(' '):])
