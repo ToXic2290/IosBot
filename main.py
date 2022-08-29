28a57570e913779fd638cf15fc0b1e8eadd2889e
ToXic2290 <derzkiy8233@gmail.com>
2022-08-29 23:42:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import sqlite3
 import configure
 
 # Авторизация в бота
-client = telebot.TeleBot(configure.config['token'])
+bot = telebot.TeleBot(configure.config['token'])
 
 
 # Команда старт и запись айди в базу данных
-@client.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     connect = sqlite3.connect('users.db')
     cursor = connect.cursor()
@@ -69,53 +69,53 @@ def start(message):
     markup.add(btn11)
     markup.add(btn14, btn15, btn20)
     markup.add(btn13)
-    client.send_message(message.chat.id, text=caption.welcome.format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=caption.welcome.format(message.from_user), reply_markup=markup)
 
     # действия на кнопочки
-@client.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "❤️‍🔥𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡❤️‍🔥"):
-        client.send_document(message.chat.id, files.whatsapp)     
+        bot.send_document(message.chat.id, files.whatsapp)     
     elif(message.text == "😈 𝕍𝕜 😈"):
-        client.send_document(message.chat.id, files.vk, caption=caption.vkd)
+        bot.send_document(message.chat.id, files.vk, caption=caption.vkd)
     elif(message.text == "🔐 𝕚𝕃𝕠𝕔𝕜 🔐"):
-        client.send_document(message.chat.id, files.ilock, caption=caption.ilockd)  
+        bot.send_document(message.chat.id, files.ilock, caption=caption.ilockd)  
     elif(message.text == '🎡 𝕀𝕟𝕤𝕥𝕒𝕘𝕣𝕒𝕞 🎡'):
-        client.send_document(message.chat.id, files.inst, caption=caption.instd)        
+        bot.send_document(message.chat.id, files.inst, caption=caption.instd)        
     elif(message.text == "💎 𝕋𝕖𝕝𝕖𝕘𝕣𝕒𝕞 💎"):
-        client.send_document(message.chat.id, files.tg, caption=caption.tgd)
+        bot.send_document(message.chat.id, files.tg, caption=caption.tgd)
     elif(message.text == "🎶 𝕊𝕡𝕠𝕥𝕚𝕗𝕪 🎶"):
-        client.send_document(message.chat.id, files.spotify, caption=caption.spd)
+        bot.send_document(message.chat.id, files.spotify, caption=caption.spd)
     elif(message.text == "🏠 𝕃𝕒𝕦𝕟𝕔𝕙𝕖𝕣 🏠"):
-        client.send_document(message.chat.id, files.launcher, caption=caption.ld)
+        bot.send_document(message.chat.id, files.launcher, caption=caption.ld)
     elif(message.text == "🎵 𝕋𝕚𝕜𝕋𝕠𝕜 🎵"):
-        client.send_document(message.chat.id, files.tt, caption=caption.ttd)
+        bot.send_document(message.chat.id, files.tt, caption=caption.ttd)
     elif(message.text == "⏰ ℂ𝕝𝕠𝕔𝕜 ⏰"):
-        client.send_document(message.chat.id, files.clock, caption=caption.cld)
+        bot.send_document(message.chat.id, files.clock, caption=caption.cld)
     elif(message.text == "😍 𝕊𝕥𝕒𝕥𝕦𝕤 𝔹𝕒𝕣 😍"):
-        client.send_document(message.chat.id, files.statusbar, caption=caption.sbd)
+        bot.send_document(message.chat.id, files.statusbar, caption=caption.sbd)
     elif(message.text == "🧷 ℂ𝕒𝕝𝕔𝕦𝕝𝕒𝕥𝕠𝕣 🧷"):
-        client.send_document(message.chat.id, files.calc, caption=caption.calcd)
+        bot.send_document(message.chat.id, files.calc, caption=caption.calcd)
     elif(message.text == "🖼️ 𝔾𝕒𝕝𝕝𝕖𝕣𝕪 🖼️"):
-        client.send_document(message.chat.id, files.gallery, caption=caption.gald)
+        bot.send_document(message.chat.id, files.gallery, caption=caption.gald)
     elif(message.text == "🎤 𝕚𝕍𝕠𝕚𝕔𝕖 🎤"):
-        client.send_document(message.chat.id, files.voice, caption=caption.voiced)
+        bot.send_document(message.chat.id, files.voice, caption=caption.voiced)
     elif(message.text == "💭 𝕄𝕖𝕤𝕤𝕒𝕘𝕖𝕤 💭"):
-        client.send_document(message.chat.id, files.msg, caption=caption.msgd)
+        bot.send_document(message.chat.id, files.msg, caption=caption.msgd)
     elif(message.text == "🙍‍♂️ ℂ𝕠𝕟𝕥𝕒𝕔𝕥𝕤 🙍‍♂️"):
-        client.send_document(message.chat.id, files.contacts, caption=caption.cntd)
+        bot.send_document(message.chat.id, files.contacts, caption=caption.cntd)
     elif(message.text == "📱 ℂ𝕒𝕝𝕝𝕤 📱"):
-        client.send_document(message.chat.id, files.call, caption=caption.calld)
+        bot.send_document(message.chat.id, files.call, caption=caption.calld)
     elif(message.text == "🎧 𝕄𝕦𝕤𝕚𝕔 🎧"):
-        client.send_document(message.chat.id, files.music, caption=caption.msd)
+        bot.send_document(message.chat.id, files.music, caption=caption.msd)
     elif(message.text == "⌨️ 𝕂𝕖𝕪𝕓𝕠𝕒𝕣𝕕 ⌨️"):
-        client.send_document(message.chat.id, files.kb, caption=caption.kbd)
+        bot.send_document(message.chat.id, files.kb, caption=caption.kbd)
     elif(message.text == "📒 ℕ𝕠𝕥𝕖𝕤 📒"):
-        client.send_document(message.chat.id, files.notes, caption=caption.ntd)
+        bot.send_document(message.chat.id, files.notes, caption=caption.ntd)
     elif(message.text == "💌ℕ𝕠𝕥𝕚𝕗𝕚𝕔𝕒𝕥𝕚𝕠𝕟𝕤💌"):
-        client.send_document(message.chat.id, files.notify, caption=caption.ntf)
+        bot.send_document(message.chat.id, files.notify, caption=caption.ntf)
     elif(message.text == "🧭 𝕚ℂ𝕠𝕞𝕡𝕒𝕤𝕤 🧭"):
-        client.send_document(message.chat.id, files.cmps, caption=caption.cmpsd)
+        bot.send_document(message.chat.id, files.cmps, caption=caption.cmpsd)
     
 
     # инфа обо мне и повторная попытка записи в бд если человек не нажимал старт после обновы
@@ -146,14 +146,14 @@ def func(message):
         btn4 = types.KeyboardButton("💸 Донат")
         back = types.KeyboardButton("⛺️ Домой")
         markup.add(btn1, btn3, btn4, back)
-        client.send_message(message.chat.id, text="😙 Тут вы можете узнать некоторую информацию о создателе", reply_markup=markup)
+        bot.send_message(message.chat.id, text="😙 Тут вы можете узнать некоторую информацию о создателе", reply_markup=markup)
 
     elif(message.text == "🫠 Биография"):
-        client.send_message(message.chat.id, caption.bio, parse_mode='MarkdownV2')
+        bot.send_message(message.chat.id, caption.bio, parse_mode='MarkdownV2')
     elif message.text == "💸 Донат":
-        client.send_message(message.chat.id, caption.donate)
+        bot.send_message(message.chat.id, caption.donate)
     elif message.text == "🔗 Социальные сети":
-        client.send_message(message.chat.id, caption.soc)
+        bot.send_message(message.chat.id, caption.soc)
     elif (message.text == "⛺️ Домой"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("😈 𝕍𝕜 😈")
@@ -190,9 +190,9 @@ def func(message):
         markup.add(btn11)
         markup.add(btn14, btn15, btn20)
         markup.add(btn13)
-        client.send_message(message.chat.id, text='''✨ Телепорт в главное меню прошел успешно 😊'''.format(message.from_user), reply_markup=markup)
+        bot.send_message(message.chat.id, text='''✨ Телепорт в главное меню прошел успешно 😊'''.format(message.from_user), reply_markup=markup)
     else:
-        client.send_message(message.chat.id, text="😔 Простите, но.. я не знаю что вам ответить на это...")
+        bot.send_message(message.chat.id, text="😔 Простите, но.. я не знаю что вам ответить на это...")
      
 
 
@@ -200,4 +200,4 @@ def func(message):
 
 
 # вечная работа бота
-client.infinity_polling()
+bot.infinity_polling()
